a7b93653d25a2681158ee65ec6e68b5dc7373f2b
Adrian Murawski <adrianmurawski79@gmail.com>
2024-11-17 23:32:54 +0100

    necessary changes in datagram size convention

--- zadanie1.1/python/client/udp_client.py
+++ zadanie1.1/python/client/udp_client.py
@@ -5,28 +5,29 @@ import sys
 
 def start_client(host='localhost', port=12345):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sizes = [1, 100, 200, 500, 1000, 2000, 4000]
+    sizes = [1, 2, 3, 4, 5, 10, 20, 50] # tablica rozmiarów przesyłanych wiaodmości
     # sizes = list(range(4090, 4100, 1)) # nie zadziała poprawnie dla wartości większych niż 4096 - bo taki rozmiar bufora w serwerze
     # sizes = list(range(65500, 65510)) # nie zostanie obsłużone - bo system nie obsługuje wiadomości powyżej 65507 bajtów
 
     for size in sizes:
-        length = (max(size-2, 0)).to_bytes(2, byteorder='big') # 2 pierwsze bajty na rozmiar treści
-        content = bytes([(65 + (i % 26)) for i in range(size-2)]) # stworzenie wiadomości
+        length = (size).to_bytes(2, byteorder='big') # 2 pierwsze bajty na rozmiar treści
+        content = bytes([(65 + (i % 26)) for i in range(size)]) # stworzenie wiadomości
         message = length + content
 
-        print(f"Wysyłanie wiadomości - rozmiar zawartości {size} bajtów...")
+        print(f"Wysyłanie wiadomości - rozmiar wiadomości {size}B (całego datagramu {size+2}B)...")
         client_socket.sendto(message, (host, port))
 
         try:
             client_socket.settimeout(1)
             response, _ = client_socket.recvfrom(128)
             if response == b"OK":
-                print(f"Otrzymano potwierdzenie dla datagramu o rozmiarze {size} bajtów")
+                print(f"Otrzymano potwierdzenie dla wiadomości o rozmiarze {size}B (całego datagramu {size+2}B)")
             else:
                 print(response.decode('utf-8'))
         except socket.timeout:
             print("Brak odpowiedzi")
             break
+        print()
 
         time.sleep(1)
 
--- zadanie1.1/python/server/udp_server.py
+++ zadanie1.1/python/server/udp_server.py
@@ -17,13 +17,13 @@ def start_server(host='pserver', port=12345):
 
         if len(content) == length:
             if all(c == b'A'[0] + (i % 26) for i, c in enumerate(content)):
-                print(f"Odebrano poprawny datagram od {address} o rozmiarze {len(data)} bajtów")
+                print(f"Odebrano poprawny datagram od {address} o rozmiarze wiadomości {len(content)}B (cały datagram {len(content)+2}B)")
                 server_socket.sendto(b"OK", address)
             else:
                 print(f"Odebrano błędny datagram od {address} - dane uległy zmianie")
                 server_socket.sendto(b"Wrong data", address)
         else:
-            print(f"Odebrano błędny datagram od {address} - [({len(content)=}) != ({length=})]")
+            print(f"Odebrano błędny datagram od {address} - [({len(content)=})B != ({length=})B]")
             server_socket.sendto(b"Odebrano niepoprawna dlugosc datagramu", address)
 
 
