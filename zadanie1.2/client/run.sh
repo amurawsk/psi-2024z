93be8f61de1f859c1c9cbe9fa796d4af53317307
Adrian Murawski <adrianmurawski79@gmail.com>
2024-11-23 18:47:26 +0100

    replaced run scripts with docker compose

--- zadanie1.2/client/run.sh
+++
@@ -1,2 +0,0 @@
-docker build -t pclient .
-docker run -it --network z39_network --name pclient pclient pserver 8001
--- zadanie1.2/server/run.sh
+++
@@ -1,2 +0,0 @@
-docker build -t pserver .
-docker run -it --network-alias pserver --hostname pserver --network z39_network --name pserver pserver 8001
