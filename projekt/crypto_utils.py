4194c2309cf5bfe62bfd88b47ac6768f373e2884
Adrian Murawski <adrianmurawski79@gmail.com>
2025-01-12 22:11:27 +0100

    changed hmac data

--- projekt/crypto_utils.py
+++ projekt/crypto_utils.py
@@ -3,35 +3,36 @@ from Crypto.Util.Padding import pad, unpad
 from Crypto.Hash import HMAC, SHA256
 
 
-def generate_hmac(message, secret_key):
-    hmac = HMAC.new(secret_key.encode(), message, SHA256)
+def generate_hmac(data, secret_key):
+    hmac = HMAC.new(secret_key.encode(), data, SHA256)
     return hmac.digest()
 
 
-def verify_hmac(message, hmac_to_verify, secret_key):
-    generated_hmac = HMAC.new(secret_key.encode(), message, SHA256)
-    return generated_hmac.digest() == hmac_to_verify
+def verify_hmac(data, hmac_to_verify, secret_key):
+    try:
+        hmac = HMAC.new(secret_key.encode(), data, SHA256)
+        hmac.verify(hmac_to_verify)
+        return True
+    except ValueError:
+        return False
 
 
 def get_encrypted_message(aes_key, message, shared_key):
     cipher = AES.new(aes_key, AES.MODE_CBC)
+    iv = cipher.iv
     encrypted_message = cipher.encrypt(pad(message.encode(), AES.block_size))
-    hmac = generate_hmac(encrypted_message, str(shared_key))
-    return hmac + cipher.iv + encrypted_message
+    hmac = generate_hmac(iv + encrypted_message, str(shared_key))
+    return hmac + iv + encrypted_message
 
 
 def get_decrypted_message(aes_key, encrypted_data, shared_key):
     hmac_len = SHA256.digest_size
     iv_len = AES.block_size
     hmac_received = encrypted_data[:hmac_len]
-    iv = encrypted_data[hmac_len : hmac_len + iv_len]
-    encrypted_message = encrypted_data[hmac_len + iv_len :]
-
-    if not verify_hmac(encrypted_message, hmac_received, str(shared_key)):
-        raise ValueError(
-            "Weryfikacja HMAC nie powiodła się. Wiadomość została zmodyfikowana."
-        )
-
+    iv = encrypted_data[hmac_len:hmac_len + iv_len]
+    encrypted_message = encrypted_data[hmac_len + iv_len:]
+    if not verify_hmac(iv + encrypted_message, hmac_received, str(shared_key)):
+        raise ValueError("Weryfikacja HMAC nie powiodła się. Wiadomość została zmodyfikowana.")
     cipher = AES.new(aes_key, AES.MODE_CBC, iv)
     decrypted_data = unpad(cipher.decrypt(encrypted_message), AES.block_size)
 
